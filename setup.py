343c42d011e09947c4417d7ec64c31a8b020433f
unknown <pap0046@nhs.viv.edu.au>
2018-03-26 11:55:31 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "fortniteapi",
+    packages = ["fortniteapi"],
+    version = "1.0.0",
+    description = "A python wrapper for the fortnitetracker.com API",
+    author = "Matt P",
+    author_email = "pap0046@nhs.vic.edu.au",
+    url = "https://github.com/mattp111/fortniteapi"
+    download_url = "https://github.com/mattp111/fortniteapi/archive/1.0.0.tar.gz"
+    keywords = ["fortnite", "api", "tracker", "fortnite tracker"]
+    classifiers = [],
+)
